f024000fe4bf33d4ff93eae43acac05c704d2ce6
Luiz Torres Villalobos <112789200+luiztorres2002@users.noreply.github.com>
2025-01-23 18:12:10 -0600

    First Commit

---
+++ dream-shops/src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -0,0 +1,34 @@
+package com.dailycodework.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private BigDecimal price;
+    private int inventory;
+
+    //Clases compuestas
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+
+
+
+}
